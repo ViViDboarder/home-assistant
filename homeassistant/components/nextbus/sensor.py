home-assistant/core

NextBus: Use data update coordinator to reduce api calls

homeassistant/components/nextbus/sensor.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 from itertools import chain
 import logging
 
-from py_nextbus import NextBusClient
 import voluptuous as vol
 
 from homeassistant.components.sensor import (
@@ -14,14 +13,16 @@ from homeassistant.components.sensor import (
 )
 from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
 from homeassistant.const import CONF_NAME
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.util.dt import utc_from_timestamp
 
 from .const import CONF_AGENCY, CONF_ROUTE, CONF_STOP, DOMAIN
+from .coordinator import NextBusDataUpdateCoordinator
 from .util import listify, maybe_first
 
 _LOGGER = logging.getLogger(__name__)
@@ -66,24 +67,27 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Load values from configuration and initialize the platform."""
-    client = NextBusClient(output_format="json")
-
     _LOGGER.debug(config.data)
+    entry_agency = config.data[CONF_AGENCY]
 
-    sensor = NextBusDepartureSensor(
-        client,
-        config.data[CONF_AGENCY],
-        config.data[CONF_ROUTE],
-        config.data[CONF_STOP],
-        config.data.get(CONF_NAME),
+    coordinator: NextBusDataUpdateCoordinator = hass.data[DOMAIN].get(entry_agency)
+
+    async_add_entities(
+        (
+            NextBusDepartureSensor(
+                coordinator,
+                config.data[CONF_AGENCY],
+                config.data[CONF_ROUTE],
+                config.data[CONF_STOP],
+                config.data.get(CONF_NAME),
+            ),
+        )
     )
 
-    async_add_entities((sensor,))
 
-    await hass.async_add_executor_job(sensor.update)
-
-
-class NextBusDepartureSensor(SensorEntity):
+class NextBusDepartureSensor(
+    CoordinatorEntity[NextBusDataUpdateCoordinator], SensorEntity
+):
     """Sensor class that displays upcoming NextBus times.
 
     To function, this requires knowing the agency tag as well as the tags for
@@ -97,26 +101,37 @@ class NextBusDepartureSensor(SensorEntity):
     _attr_device_class = SensorDeviceClass.TIMESTAMP
     _attr_icon = "mdi:bus"
 
-    def __init__(self, client, agency, route, stop, name=None):
+    def __init__(
+        self,
+        coordinator: NextBusDataUpdateCoordinator,
+        agency: str,
+        route: str,
+        stop: str,
+        name: str | None = None,
+    ) -> None:
         """Initialize sensor with all required config."""
+        super().__init__(coordinator)
         self.agency = agency
         self.route = route
         self.stop = stop
         self._custom_name = name
         # Maybe pull a more user friendly name from the API here
         self._name = f"{agency} {route}"
-        self._client = client
 
         # set up default state attributes
-        self._state = None
-        self._attributes = {}
+        self._state: str | None = None
+        self._attributes: dict[str, str] = {}
 
     def _log_debug(self, message, *args):
         """Log debug message with prefix."""
         _LOGGER.debug(":".join((self.agency, self.route, self.stop, message)), *args)
 
+    def _log_err(self, message, *args):
+        """Log error message with prefix."""
+        _LOGGER.error(":".join((self.agency, self.route, self.stop, message)), *args)
+
     @property
-    def name(self):
+    def name(self) -> str:
         """Return sensor name.
 
         Uses an auto generated name based on the data from the API unless a
@@ -128,42 +143,32 @@ class NextBusDepartureSensor(SensorEntity):
         return self._name
 
     @property
-    def native_value(self):
-        """Return current state of the sensor."""
-        return self._state
-
-    @property
-    def extra_state_attributes(self):
+    def extra_state_attributes(self) -> dict[str, str]:
         """Return additional state attributes."""
         return self._attributes
 
-    def update(self) -> None:
+    @property
+    def unique_id(self) -> str:
+        """Return unique id for the sensor based on config values."""
+        return f"{self.agency}-{self.route}-{self.stop}"
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
         """Update sensor with new departures times."""
-        # Note: using Multi because there is a bug with the single stop impl
-        results = self._client.get_predictions_for_multi_stops(
-            [{"stop_tag": self.stop, "route_tag": self.route}], self.agency
-        )
+        results = self.coordinator.get_prediction_data(self.stop, self.route)
 
         self._log_debug("Predictions results: %s", results)
 
-        if "Error" in results:
-            self._log_debug("Could not get predictions: %s", results)
-
-        if not results.get("predictions"):
-            self._log_debug("No predictions available")
-            self._state = None
-            # Remove attributes that may now be outdated
-            self._attributes.pop("upcoming", None)
+        if not results or "Error" in results:
+            self._log_err("Error getting predictions: %s", str(results))
             return
-
-        results = results["predictions"]
 
         # Set detailed attributes
         self._attributes.update(
             {
-                "agency": results.get("agencyTitle"),
-                "route": results.get("routeTitle"),
-                "stop": results.get("stopTitle"),
+                "agency": str(results.get("agencyTitle")),
+                "route": str(results.get("routeTitle")),
+                "stop": str(results.get("stopTitle")),
             }
         )
 
@@ -190,16 +195,18 @@ class NextBusDepartureSensor(SensorEntity):
         # Short circuit if we don't have any actual bus predictions
         if not predictions:
             self._log_debug("No upcoming predictions available")
-            self._state = None
+            self._attr_native_value = None
             self._attributes["upcoming"] = "No upcoming predictions"
-            return
 
-        # Generate list of upcoming times
-        self._attributes["upcoming"] = ", ".join(
-            sorted((p["minutes"] for p in predictions), key=int)
-        )
+        else:
+            # Generate list of upcoming times
+            self._attributes["upcoming"] = ", ".join(
+                sorted((p["minutes"] for p in predictions), key=int)
+            )
 
-        latest_prediction = maybe_first(predictions)
-        self._state = utc_from_timestamp(int(latest_prediction["epochTime"]) / 1000)
+            latest_prediction = maybe_first(predictions)
+            self._attr_native_value = utc_from_timestamp(
+                int(latest_prediction["epochTime"]) / 1000
+            )
 
         self.async_write_ha_state()

ViViDboarder/home-assistant

homeassistant/components/nextbus/sensor.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 from itertools import chain
 import logging
 
-from py_nextbus import NextBusClient
 import voluptuous as vol
 
 from homeassistant.components.sensor import (
@@ -14,14 +13,16 @@ from homeassistant.components.sensor import (
 )
 from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
 from homeassistant.const import CONF_NAME
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.util.dt import utc_from_timestamp
 
 from .const import CONF_AGENCY, CONF_ROUTE, CONF_STOP, DOMAIN
+from .coordinator import NextBusDataUpdateCoordinator
 from .util import listify, maybe_first
 
 _LOGGER = logging.getLogger(__name__)
@@ -66,23 +67,28 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Load values from configuration and initialize the platform."""
-    client = NextBusClient(output_format="json")
-
     _LOGGER.debug(config.data)
+    entry_agency = config.data[CONF_AGENCY]
 
-    sensor = NextBusDepartureSensor(
-        client,
-        config.entry_id,
-        config.data[CONF_AGENCY],
-        config.data[CONF_ROUTE],
-        config.data[CONF_STOP],
-        config.data.get(CONF_NAME),
+    coordinator: NextBusDataUpdateCoordinator = hass.data[DOMAIN].get(entry_agency)
+
+    async_add_entities(
+        (
+            NextBusDepartureSensor(
+                coordinator,
+                config.entry_id,
+                config.data[CONF_AGENCY],
+                config.data[CONF_ROUTE],
+                config.data[CONF_STOP],
+                config.data.get(CONF_NAME),
+            ),
+        ),
     )
 
-    async_add_entities((sensor,), True)
 
-
-class NextBusDepartureSensor(SensorEntity):
+class NextBusDepartureSensor(
+    CoordinatorEntity[NextBusDataUpdateCoordinator], SensorEntity
+):
     """Sensor class that displays upcoming NextBus times.
 
     To function, this requires knowing the agency tag as well as the tags for
@@ -96,12 +102,21 @@ class NextBusDepartureSensor(SensorEntity):
     _attr_device_class = SensorDeviceClass.TIMESTAMP
     _attr_icon = "mdi:bus"
 
-    def __init__(self, client, entry_id, agency, route, stop, name=None):
+    def __init__(
+        self,
+        coordinator: NextBusDataUpdateCoordinator,
+        entry_id: str,
+        agency: str,
+        route: str,
+        stop: str,
+        name: str | None = None,
+    ) -> None:
         """Initialize sensor with all required config."""
+        super().__init__(coordinator)
         self.agency = agency
         self.route = route
         self.stop = stop
-        self._attr_extra_state_attributes = {}
+        self._attr_extra_state_attributes: dict[str, str] = {}
         self._attr_unique_id = entry_id
 
         # Maybe pull a more user friendly name from the API here
@@ -109,40 +124,39 @@ class NextBusDepartureSensor(SensorEntity):
         if name:
             self._attr_name = name
 
-        self._client = client
-
     def _log_debug(self, message, *args):
         """Log debug message with prefix."""
         _LOGGER.debug(":".join((self.agency, self.route, self.stop, message)), *args)
 
-    def update(self) -> None:
+    def _log_err(self, message, *args):
+        """Log error message with prefix."""
+        _LOGGER.error(":".join((self.agency, self.route, self.stop, message)), *args)
+
+    @property
+    def unique_id(self) -> str:
+        """Return unique id for the sensor based on config values."""
+        return f"{self.agency}-{self.route}-{self.stop}"
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
         """Update sensor with new departures times."""
-        # Note: using Multi because there is a bug with the single stop impl
-        results = self._client.get_predictions_for_multi_stops(
-            [{"stop_tag": self.stop, "route_tag": self.route}], self.agency
-        )
+        results = self.coordinator.get_prediction_data(self.stop, self.route)
+        self._attr_attribution = self.coordinator.get_attribution()
 
         self._log_debug("Predictions results: %s", results)
-        self._attr_attribution = results.get("copyright")
 
-        if "Error" in results:
-            self._log_debug("Could not get predictions: %s", results)
-
-        if not results.get("predictions"):
-            self._log_debug("No predictions available")
+        if not results or "Error" in results:
+            self._log_err("Error getting predictions: %s", str(results))
             self._attr_native_value = None
-            # Remove attributes that may now be outdated
             self._attr_extra_state_attributes.pop("upcoming", None)
             return
-
-        results = results["predictions"]
 
         # Set detailed attributes
         self._attr_extra_state_attributes.update(
             {
-                "agency": results.get("agencyTitle"),
-                "route": results.get("routeTitle"),
-                "stop": results.get("stopTitle"),
+                "agency": str(results.get("agencyTitle")),
+                "route": str(results.get("routeTitle")),
+                "stop": str(results.get("stopTitle")),
             }
         )
 
@@ -171,14 +185,15 @@ class NextBusDepartureSensor(SensorEntity):
             self._log_debug("No upcoming predictions available")
             self._attr_native_value = None
             self._attr_extra_state_attributes["upcoming"] = "No upcoming predictions"
-            return
+        else:
+            # Generate list of upcoming times
+            self._attr_extra_state_attributes["upcoming"] = ", ".join(
+                sorted((p["minutes"] for p in predictions), key=int)
+            )
 
-        # Generate list of upcoming times
-        self._attr_extra_state_attributes["upcoming"] = ", ".join(
-            sorted((p["minutes"] for p in predictions), key=int)
-        )
+            latest_prediction = maybe_first(predictions)
+            self._attr_native_value = utc_from_timestamp(
+                int(latest_prediction["epochTime"]) / 1000
+            )
 
-        latest_prediction = maybe_first(predictions)
-        self._attr_native_value = utc_from_timestamp(
-            int(latest_prediction["epochTime"]) / 1000
-        )
+        self.async_write_ha_state()